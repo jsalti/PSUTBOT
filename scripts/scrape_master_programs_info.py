e67557cc2d2f7b0e47458c34651f7ff94dd777a6
jsalti <104867354+jsalti@users.noreply.github.com>
2023-12-24 17:32:02 +0300

    Update scrape_master_programs_info.py

--- scripts/scrape_master_programs_info.py
+++ scripts/scrape_master_programs_info.py
@@ -9,70 +9,65 @@ Original file is located at
 
 import os
 import requests
-import fitz  # PyMuPDF
-import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import chromedriver_autoinstaller
-import time
-import json
-
-import requests
 from bs4 import BeautifulSoup
 import json
-import os
 
-def scrape_master_programs_info(url):
-    response = requests.get(url)
-    html_content = response.text
+def scrape_master_programs_from_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for bad responses
+        html_content = response.text
 
-    # Parse the HTML content using BeautifulSoup
-    soup_master_programs = BeautifulSoup(html_content, "html.parser")
+        # Parse the HTML content using BeautifulSoup
+        soup_master_programs = BeautifulSoup(html_content, "html.parser")
 
-    # Find the container for the program information
-    program_container = soup_master_programs.find("div", class_="tab-pane fade show active")
+        # Find the container for the program information
+        program_container = soup_master_programs.find("div", class_="tab-pane fade show active")
 
-    # Extract information about programs
-    programs = program_container.find_all("a", href=True)
+        # Extract information about programs
+        programs = program_container.find_all("a", href=True)
 
-    program_data = []
-    for program in programs:
-        program_name_tag = program.find("h4")
-        program_description_tag = program.find("p")
+        program_data = []
+        for program in programs:
+            name_tag = program.find("h4")
+            description_tag = program.find("p")
 
-        if program_name_tag and program_description_tag:
-            program_name = program_name_tag.text.strip()
-            program_description = program_description_tag.text.strip()
-            program_link = program.get("href")
+            if name_tag and description_tag:
+                program_name = name_tag.text.strip()
+                program_description = description_tag.text.strip()
+                program_link = program.get("href")
 
-            program_data.append({
-                "Program Name": program_name,
-                "Description": program_description,
-                "Link": program_link
-            })
+                program_data.append({
+                    "Program Name": program_name,
+                    "Description": program_description,
+                    "Link": program_link
+                })
 
-    return program_data
+        return program_data
+    except Exception as e:
+        print(f"Error scraping master's programs: {e}")
+        return None
 
 # Example URL
 url_master_programs = "https://psut.edu.jo/en/school/School_of_Graduate_Studies_Scientific_Research"
 
 # Call the function to scrape master's programs information
-master_programs_data = scrape_master_programs_info(url_master_programs)
+master_programs_data = scrape_master_programs_from_url(url_master_programs)
 
-# Specify the directory where you have write permissions
-output_directory = '/tmp/output_masters'
+if master_programs_data is not None:
+    # Specify the directory where you have write permissions
+    output_directory = '/tmp/output_masters'
 
-# Ensure the directory exists
-os.makedirs(output_directory, exist_ok=True)
+    # Ensure the directory exists
+    os.makedirs(output_directory, exist_ok=True)
 
-# Specify the output JSON file path
-output_json_file = os.path.join(output_directory, 'master_programs_data.json')
+    # Specify the output JSON file path
+    output_json_file = os.path.join(output_directory, 'master_programs_data.json')
 
-# Save the extracted data to a JSON file
-with open(output_json_file, 'w', encoding='utf-8') as json_file:
-    json.dump(master_programs_data, json_file, ensure_ascii=False, indent=2)
+    # Save the extracted data to a JSON file
+    with open(output_json_file, 'w', encoding='utf-8') as json_file:
+        json.dump(master_programs_data, json_file, ensure_ascii=False, indent=2)
 
-print(f'Data saved to: {output_json_file}')
+    print(f'Data saved to: {output_json_file}')
+else:
+    print("No data to save.")
