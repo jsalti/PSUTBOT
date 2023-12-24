fa3300334a711c1c5219fe2f74466f8f50fc03c8
jsalti <104867354+jsalti@users.noreply.github.com>
2023-12-24 17:30:21 +0300

    Update extract_academic_calendar_data.py

--- scripts/extract_academic_calendar_data.py
+++ scripts/extract_academic_calendar_data.py
@@ -18,39 +18,34 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import chromedriver_autoinstaller
 
-def extract_calandar_data(pdf_url):
-    # Download the PDF
-    response = requests.get(pdf_url)
+def download_pdf(pdf_url, destination_path):
+    try:
+        response = requests.get(pdf_url)
+        with open(destination_path, 'wb') as pdf_file:
+            pdf_file.write(response.content)
+        return True
+    except Exception as e:
+        print(f"Error downloading PDF: {e}")
+        return False
 
-    # Create a "static/" directory in the current working directory
-    static_directory = os.path.join(os.getcwd(), 'static')
-    os.makedirs(static_directory, exist_ok=True)
-
-    # Specify the absolute path for the PDF file
-    pdf_file_path = os.path.join(static_directory, 'academic_calendar.pdf')
-    with open(pdf_file_path, 'wb') as pdf_file:
-        pdf_file.write(response.content)
-
-    # Use PyMuPDF to extract text from the PDF
-    pdf_document = fitz.open(pdf_file_path)
-    data = []
-    for page_number in range(pdf_document.page_count):
-        page = pdf_document[page_number]
-        lines = page.get_text("text").splitlines()
-        formatted_text = ' '.join(lines)
-        data.append(formatted_text)
-
-    # Close the PDF document
-    pdf_document.close()
-
-    # Return the extracted text data
-    return data
+def extract_pdf_text(pdf_path):
+    try:
+        pdf_document = fitz.open(pdf_path)
+        data = []
+        for page_number in range(pdf_document.page_count):
+            page = pdf_document[page_number]
+            lines = page.get_text("text").splitlines()
+            formatted_text = ' '.join(lines)
+            data.append(formatted_text)
+        pdf_document.close()
+        return data
+    except Exception as e:
+        print(f"Error extracting text from PDF: {e}")
+        return None
 
 # Set up the WebDriver with Chrome options
 chrome_options = Options()
 chrome_options.add_argument('--lang=en')
-
-# Use chromedriver_autoinstaller to ensure the latest Chromedriver is installed
 chromedriver_autoinstaller.install()
 
 # Initialize the WebDriver
@@ -77,20 +72,25 @@ second_button = WebDriverWait(driver, 20).until(
 # Get the PDF URL from the second button's href attribute
 pdf_url = second_button.get_attribute('href')
 
-# Extract data from the PDF
-data = extract_calandar_data(pdf_url)
-
-# Close the WebDriver when done
-driver.quit()
-
 # Specify an alternative writable directory
 output_directory = '/tmp/output_academic_calendar'
 os.makedirs(output_directory, exist_ok=True)
 
-# Save the extracted data to a JSON file in the specified directory
-json_file_path = os.path.join(output_directory, 'extracted_data.json')
-with open(json_file_path, 'w', encoding='utf-8') as json_file:
-    json.dump(data, json_file, ensure_ascii=False, indent=2)
+# Specify the absolute path for the PDF file
+pdf_file_path = os.path.join(output_directory, 'academic_calendar.pdf')
 
-# Print the path to the saved JSON file
-print(f'Data saved to: {json_file_path}')
+# Download the PDF
+if download_pdf(pdf_url, pdf_file_path):
+    # Extract data from the PDF
+    data = extract_pdf_text(pdf_file_path)
+
+    # Save the extracted data to a JSON file in the specified directory
+    json_file_path = os.path.join(output_directory, 'extracted_data.json')
+    with open(json_file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=2)
+
+    # Print the path to the saved JSON file
+    print(f'Data saved to: {json_file_path}')
+
+# Close the WebDriver when done
+driver.quit()
