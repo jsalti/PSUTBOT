6fc590560bb5af79ea593e256a0fcd59f48db53b
jsalti <104867354+jsalti@users.noreply.github.com>
2024-01-17 13:08:40 +0300

    Update insert_faq_to_mongodb.py

--- database/insert_faq_to_mongodb.py
+++ database/insert_faq_to_mongodb.py
@@ -6,7 +6,7 @@ db = client['PSUTBOT']
 
 def insert_faq_to_mongodb(data, db, collection_name):
     """
-    Inserts FAQ data into MongoDB.
+    Inserts or updates FAQ data into MongoDB.
 
     Parameters:
         data (list): List of dictionaries containing FAQ data.
@@ -14,12 +14,18 @@ def insert_faq_to_mongodb(data, db, collection_name):
         collection_name (str): Name of the collection to insert data into.
     """
     # Create a collection
-    collection_faq = db[collection_name]
+    collection = db[collection_name]
 
-    # Insert JSON data into MongoDB
-    collection_faq.insert_many(data)
+    # Update or insert data into MongoDB using UpdateOne with upsert
+    operations = [UpdateOne(
+        {"FAQ Question": item["FAQ Question"]},
+        {"$set": item},
+        upsert=True
+    ) for item in data]
 
-    print("FAQ data inserted successfully!")
+    collection.bulk_write(operations)
+    print("FAQ data inserted or updated successfully into MongoDB!")
+
 
 # URLs of the pages you want to scrape
 urls_list = [
@@ -31,7 +37,7 @@ urls_list = [
 
 # Collect data for all URLs
 all_faq_data = []
-for i, url in enumerate(urls_list, 1):
+for url in urls_list:
     result_faq_data = scrape_faq_info(url)
     if result_faq_data:
         all_faq_data.extend(result_faq_data)
@@ -54,11 +60,7 @@ print(f'Data saved to: {json_file_path}')
 # Insert FAQ data into MongoDB using the function
 collection_name_faq = 'faq_information'  # Update with your actual collection name
 
-# Load the JSON data from the file as a list
-with open(json_file_path, 'r', encoding='utf-8') as file:
-    faq_data_list = json.load(file)
+# Call the function to insert or update data into MongoDB
+insert_faq_to_mongodb(all_faq_data, db, collection_name_faq)
 
-# Call the function to insert data into MongoDB
-insert_faq_to_mongodb(faq_data_list, db, collection_name_faq)
-
-print("FAQ data inserted successfully into MongoDB!")
+print("FAQ data inserted or updated successfully into MongoDB!")
